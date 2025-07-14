996944cfcce4e624e9f826bd2283865cb779ba3c
Verzional <verzional777@gmail.com>
2025-07-14 11:47:08 +0800

    Button Fix

--- src/app/dashboard/pr/[id]/edit/page.tsx
+++ src/app/dashboard/pr/[id]/edit/page.tsx
@@ -15,7 +15,7 @@ export default async function EditActivityPage(props: {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-6">Create New Achievement</h1>
+      <h1 className="text-3xl font-bold mb-6">Edit Achievement</h1>
       <AchievementForm mode="edit" data={achievement} />
     </div>
   );
